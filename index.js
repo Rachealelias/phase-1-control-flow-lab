de20b5b92589f53c1dea4eee6e04538007a24ef7
Rachealelias <rachealsamuel708@gmail.com>
2021-06-30 14:55:39 -0400

    commit tree 4930ba970618e4d4898f5c1e722f5cf37cd56ba2

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return "This one is on me!";
+  }
+ 
+  else if(distance <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else{
+    return "No can do.";
+  }
 }
+scuberGreetingForFeet();
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  console.log(city)
+  const message = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  //if(city === 'NYC'){
+    //return 'Ok, sounds good.';
+  //}else{
+    //return 'No go.'
+  //}
+  return message;
 }
+ternaryCheckCity();
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let text;
+switch(tip){
+  case 'generous':
+  text= 'Thank you so much.';
+    break;
+    case 'not as generous':
+     text= 'Thank you.';
+      break;
+      case 'thanks for everything':
+        text= 'Bye.'
+ }
+ return text;
 }
